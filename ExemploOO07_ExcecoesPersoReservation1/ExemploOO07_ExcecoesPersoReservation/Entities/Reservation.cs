9acab74b58b99ec2c042f6a2dc3108f40120196e
Talison Moura <talisonmoura13@gmail.com>
2023-01-25 16:45:28 -0300

    Add files via upload

---
+++ ExemploOO07_ExcecoesPersoReservation1/ExemploOO07_ExcecoesPersoReservation/Entities/Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploOO07_ExcecoesPersoReservation.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update must be future dates!";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Check-Out date must be after Check-In!";
+            }
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+        public override string ToString()
+        {
+            return ($"Room: {RoomNumber}, Check-In: {CheckIn.ToString("dd/MM/yyyy")}," +
+                $" Check-Out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights.");
+        }
+    }
+}
---
+++ ExemploOO07_ExcecoesPersoReservation1/ExemploOO07_ExcecoesPersoReservation/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExemploOO07_ExcecoesPersoReservation.Entities;
+namespace ExemploOO07_ExcecoesPersoReservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // TRATAMENTO DE EXCECAO COM METODOS E FUNCOES SEM BOAS PRATICAS, SEMANTICA SOBRECARREGADA
+            // TRATAMENTO REALIZADO COM METODOS RETORNANDO STRING, E LÓGICA ANINHADA. FORA DAS BOAS PRATICAS.
+            
+
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-In date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-Out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in Reservation: Check-Out date must be after Check-In!");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                if (error != null)
+                {
+                    Console.WriteLine($"Error in reservation: {error}");
+                }
+                else
+                {
+                    Console.WriteLine($"Reservation: {reservation}");
+                }
+            }
+        }
+    }
+}
