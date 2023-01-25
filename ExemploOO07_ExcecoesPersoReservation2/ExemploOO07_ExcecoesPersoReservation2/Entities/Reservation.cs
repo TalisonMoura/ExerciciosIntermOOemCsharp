b66e4c456c26100b3534cc83a59a51eb69cb6487
Talison Moura <talisonmoura13@gmail.com>
2023-01-25 17:53:53 -0300

    Add files via upload

---
+++ ExemploOO07_ExcecoesPersoReservation2/ExemploOO07_ExcecoesPersoReservation2/Entities/Reservation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExemploOO07_ExcecoesPersoReservation2.Entities.Exceptions;
+
+namespace ExemploOO07_ExcecoesPersoReservation2.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-Out date must be after Check-In!");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates!");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-Out date must be after Check-In!");
+            }
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public override string ToString()
+        {
+            return ($"Room: {RoomNumber}, Check-In: {CheckIn.ToString("dd/MM/yyyy")}," +
+                $" Check-Out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights.");
+        }
+    }
+}
---
+++ ExemploOO07_ExcecoesPersoReservation2/ExemploOO07_ExcecoesPersoReservation2/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExemploOO07_ExcecoesPersoReservation2.Entities;
+using ExemploOO07_ExcecoesPersoReservation2.Entities.Exceptions;
+
+namespace ExemploOO07_ExcecoesPersoReservation2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error in reservation: {e.Message}");
+            }
+            catch(FormatException e) 
+            {
+                Console.WriteLine($"Format Error: {e.Message}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Unexpected Erros: {e.Message}");
+            }
+        }
+    }
+}
