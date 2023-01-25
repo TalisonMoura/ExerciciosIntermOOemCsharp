b79a5471c92b807579e8d2612d9e01676d15d1ce
Talison Moura <talisonmoura13@gmail.com>
2023-01-25 16:44:29 -0300

    Add files via upload

---
+++ ExemploOO05_TryCatch/ExemploOO05_TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExemploOO05_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                int result = a / b;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed!");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine($"Format Error! {e.Message}");
+            }
+        }
+    }
+}
---
+++ ExemploOO06_BlocoFinally/ExemploOO06_BlocoFinally/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExemploOO06_BlocoFinally;
+internal class Program
+{
+    public static void Main()
+    {
+        FileStream fs = null;
+        try
+        {
+            fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (fs != null)
+            {
+                fs.Close();
+            }
+        }
+    }
+}
