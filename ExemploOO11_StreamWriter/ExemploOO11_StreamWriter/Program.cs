8b58160b3f43f199a4a02d4208bc4aa9626dd39c
Talison Moura <talisonmoura13@gmail.com>
2023-01-26 13:38:51 -0300

    Add files via upload

---
+++ ExemploOO11_StreamWriter/ExemploOO11_StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExemploOO11_StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcepath = @"C:\temp\File1.txt"; // ARQUIVO DE ORIGEM
+            string targetpath = @"C:\temp\File2.txt"; // ARQUIVO DE DESTINO  
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
---
+++ ExemploOO12_DirectoryEDirectoryInfo/ExemploOO12_DirectoryEDirectoryInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExemploOO12_DirectoryEDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\MyFolder";
+
+            try
+            {
+                // MANEIRA SIMPLIFICADA DE IENUMERABLE, A PROPRIA IDE FAZ A INFERÊNCIA DE TIPO.
+                // var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); 
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string fo in folders)
+                {
+                    Console.WriteLine(fo);
+                }
+                var fiels = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FIELS:");
+                foreach (string fi in fiels)
+                {
+                    Console.WriteLine(fi);
+                }
+                Directory.CreateDirectory(path + @"\NewFolder"); // OR Directory.CreateDirectory(@"C:\temp\MyFolder\NewFolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
