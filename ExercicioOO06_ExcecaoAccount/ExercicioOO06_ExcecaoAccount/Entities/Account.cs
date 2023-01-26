14e70adb612ad3b30d0583ae4dd875ae51241fd9
Talison Moura <talisonmoura13@gmail.com>
2023-01-25 21:48:19 -0300

    Add files via upload

---
+++ ExercicioOO06_ExcecaoAccount/ExercicioOO06_ExcecaoAccount/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioOO06_ExcecaoAccount.Entities.Exceptions;
+
+namespace ExercicioOO06_ExcecaoAccount.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimt { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withDrawLimt)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimt = withDrawLimt;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimt)
+            {
+                throw new DomainException("The amount exceeds withdraw limit!");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance!");
+            }
+
+            Balance -= amount;
+
+        }
+    }
+}
---
+++ ExercicioOO06_ExcecaoAccount/ExercicioOO06_ExcecaoAccount/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioOO06_ExcecaoAccount.Entities;
+using ExercicioOO06_ExcecaoAccount.Entities.Exceptions;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioOO06_ExcecaoAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int Number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string Holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double Balance = double.Parse(Console.ReadLine(), ci);
+            Console.Write("WithDraw Limit: ");
+            double WithDrawLimit = double.Parse(Console.ReadLine(), ci);
+            Console.WriteLine();
+
+            Account account = new Account(Number, Holder, Balance, WithDrawLimit);
+
+            Console.Write("Enter the amount for WithDraw: ");
+            double Amount = double.Parse(Console.ReadLine(), ci);
+            try
+            {
+                account.WithDraw(Amount);
+                Console.Write($"New Balance: {account.Balance.ToString("f2", ci)}");
+                Console.WriteLine();
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"WithDraw Error: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format Error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected Error: {e.Message}");
+            }
+        }
+    }
+}
