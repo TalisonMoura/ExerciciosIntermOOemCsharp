7095e88ab0a995783281323e7c16ef50e8cd2984
Talison Moura <talisonmoura13@gmail.com>
2023-01-26 15:38:24 -0300

    Add files via upload

---
+++ ExemploOO13_Path/ExemploOO13_Path/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExemploOO13_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\temp\MyFolder\File1.txt.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar); // FUNÇÃO PARA VERIFICAR CARACTER DE SEPARAÇÃO.
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator); // FUNÇÃO PARA VERIFICAR CARACTER DE SEPARAÇÃO DE PASTA.
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); // FUNÇÃO PARA VERIFICAR O ENDEREÇO DA PASTA.
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path)); // FUNÇÃO PARA VERIFICAR O NOME DO ARQUIVO E SUA EXTENSÃO.
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path)); // FUNÇÃO PARA VERIFICAR SOMENTE A EXTENSÃO DO ARQUIVO.
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path)); // FUNÇÃO PARA VERIFICAR O NOME DO ARQUIVO SEM A EXTENSÃO DO MESMO.
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path)); // FUNÇÃO PARA VERIFICAR O NOME COMPLETO DO ARQUIVO E SUA EXTENSÃO.
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath()); // FUNÇÃO PARA VERIFICAR PASTA TEMPORARIA DO SISTEMA.
+        }
+    }
+}
