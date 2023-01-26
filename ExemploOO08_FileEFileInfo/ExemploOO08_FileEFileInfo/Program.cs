5f52c2a67dccc105492492b7cb1c6e9f6f754bbd
Talison Moura <talisonmoura13@gmail.com>
2023-01-25 22:43:52 -0300

    Add files via upload

---
+++ ExemploOO08_FileEFileInfo/ExemploOO08_FileEFileInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExemploOO08_FileEFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\File1.txt";
+            string targetPath = @"C:\temp\File2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error ocurred! {e.Message}");
+            }
+        }
+    }
+}
