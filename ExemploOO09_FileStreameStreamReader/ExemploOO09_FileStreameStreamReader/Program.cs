f1dbb02c03ebb6ea230b367df142f762722a2289
Talison Moura <talisonmoura13@gmail.com>
2023-01-26 13:35:56 -0300

    Add files via upload

---
+++ ExemploOO09_FileStreameStreamReader/ExemploOO09_FileStreameStreamReader/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ExemploOO09_FileStreameStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ESSA FORMA DE LEITURA É MAIS CLEAN, E É FEITA ATRAVES DO "FILE", O QUAL A INSTANCIAÇÃO DOS OBJETOS É AUTOMATICA.
+            string path = @"C:\temp\File1.txt";
+            StreamReader sr = null;
+            
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error accurred: {e.Message}");
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            
+            /*
+            string path = @"C:\temp\File1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            string path = @"C:\temp\File1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;                                             OUTRA FORMA DE FAZER LEITURA DE ARQUIVO POR MEIO DO
+                                                                                FILESTREAM E STREAMREADER.
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error accurred: {e.Message}");
+            }
+            finally
+            {
+                if(sr != null) sr.Close();
+                if(fs != null) fs.Close();
+            }
+             
+           */
+        }
+    }
+}
---
+++ ExemploOO10_BlocoUsing/ExemploOO10_BlocoUsing/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ExemploOO10_BlocoUsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // AMBOS OS CODIGOS FAZER A MESMA EXECUÇÃO, A DIFERENÇA ENTRE O PRIMEIRO PARA O SEGUNDO É A FORMA MAIS CLEAN DE ESCREVE-LO, 
+            //  E O PRIMEIRO CÓDIGO É FEITA A INSTANCIAÇÃO DIRETAMENTE PELO "FILE", ENQUANTO A SEGUNGA É NECESSARIO INSTANCIAR O FILESTREAM E O STREAMREADER.
+            // *******SINTAXE SIMPLIFICADA QUE GARANTE QUE OS OBJETOS IDISPOSABLE SERÃO FECHADOS TIPO(FONT,FILESTREAM,STREAMREADER,STREAMWRITER)******
+
+            string path = @"C:\temp\File1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            /*
+             try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            */
+        }
+    }
+}
