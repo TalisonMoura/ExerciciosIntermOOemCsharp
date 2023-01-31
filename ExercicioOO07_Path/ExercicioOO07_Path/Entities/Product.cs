9ede52f605d6e392db25716bc57a75f8a86fcdfc
Talison Moura <talisonmoura13@gmail.com>
2023-01-31 16:14:58 -0300

    Add files via upload

---
+++ ExercicioOO07_Path/ExercicioOO07_Path/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioOO07_Path.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ ExercicioOO07_Path/ExercicioOO07_Path/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioOO07_Path.Entities;
+using System.Data;
+using System.Globalization;
+
+namespace ExercicioOO07_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+                
+                Directory.CreateDirectory(targetFolderPath);
+
+                using(StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], ci);
+                        int quantity = int.Parse(fields[2]);
+                        
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine($"{product.Name}, {product.Price.ToString("f2",ci)}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
