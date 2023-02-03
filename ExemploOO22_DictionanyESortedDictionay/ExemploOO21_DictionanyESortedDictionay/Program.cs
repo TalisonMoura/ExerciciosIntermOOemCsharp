c53591b1aed39d9461624813628c09731bd65ccb
Talison Moura <talisonmoura13@gmail.com>
2023-02-03 15:10:08 -0300

    Add files via upload

---
+++ ExemploOO22_DictionanyESortedDictionay/ExemploOO21_DictionanyESortedDictionay/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ExemploOO21_DictionanyESortedDictionay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["User"] = "Maria";
+            cookies["Email"] = "maria@gmail.com";
+            cookies["Phone"] = "99712234";
+            cookies["Phone"] = "83737388";
+
+            Console.WriteLine(cookies["Email"]);
+            Console.WriteLine(cookies["Phone"]);
+
+            cookies.Remove("Email");
+            if (cookies.ContainsKey("Email"))
+            {
+                Console.WriteLine(cookies["Email"]);
+            }
+            else
+            {
+                Console.WriteLine("There isn't 'email' key");
+            }
+
+            Console.WriteLine($"Size: {cookies.Count}");
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ ExercicioOO10_ConjuntosStudents/ExercicioOO10_ConjuntosStudents/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace ExercicioOO10_ConjuntosStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            try
+            {
+                Console.Write("Hou many students for course A? ");
+                int a = int.Parse(Console.ReadLine());
+                for (int i = 0; i < a; i++)
+                {
+                    int cod = int.Parse(Console.ReadLine());
+                    set.Add(cod);
+                }
+                
+                Console.WriteLine("---------------------------------");
+                Console.Write("Hou many students for course B? ");
+                int b = int.Parse(Console.ReadLine());
+                for (int i = 0; i < b; i++)
+                {
+                    int cod = int.Parse(Console.ReadLine());
+                    set.Add(cod);
+                }
+                
+                Console.WriteLine("---------------------------------");
+                Console.Write("Hou many students for course C? ");
+                int c = int.Parse(Console.ReadLine());
+                for (int i = 0; i < c; i++)
+                {
+                    int cod = int.Parse(Console.ReadLine());
+                    set.Add(cod);
+                }
+                
+                Console.WriteLine();
+                Console.Write($"Total students: {set.Count}");
+                Console.WriteLine();
+            
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
