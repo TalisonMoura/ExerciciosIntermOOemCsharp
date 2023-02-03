33f11f9e5f813ae28b24fbbcb087f253bcc81316
Talison Moura <talisonmoura13@gmail.com>
2023-02-03 16:07:00 -0300

    Add files via upload

---
+++ ExercicioOO11_DictionaryCandidate/ExercicioOO11_DictionaryCandidate/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExercicioOO11_DictionaryCandidate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr =  File.OpenText(path))
+                {
+                    Dictionary<string, int> set = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidateName = line[0];
+                        int candidateVote = int.Parse(line[1]);
+                        
+                        if(set.ContainsKey(candidateName))
+                        {
+                            set[candidateName] += candidateVote;
+                        }
+                        else
+                        {
+                            set[candidateName] = candidateVote;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> obj in set)
+                    {
+                        Console.WriteLine($"{obj.Key}: {obj.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred {e.Message}");
+            }
+        }
+    }
+}
