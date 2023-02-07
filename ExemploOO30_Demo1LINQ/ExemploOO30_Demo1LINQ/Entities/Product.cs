8c6a6cc8c650cd8b67d1055d56db65403db97572
Talison Moura <talisonmoura13@gmail.com>
2023-02-07 17:53:20 -0300

    Add files via upload

---
+++ ExemploOO30_Demo1LINQ/ExemploOO30_Demo1LINQ/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploOO30_Demo1LINQ.Entities
+{
+    internal class Product
+    {
+        CultureInfo ci = CultureInfo.InvariantCulture;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return ($"{Id}, {Name}, {Price.ToString("f2", ci)}, {Category.Name}, {Category.Tier}");
+        }
+    }
+}
