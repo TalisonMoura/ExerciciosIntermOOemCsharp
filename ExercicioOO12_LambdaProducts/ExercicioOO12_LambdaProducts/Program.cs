4298653fd655016637e83295c5342c9b45419271
Talison Moura <talisonmoura13@gmail.com>
2023-02-07 18:52:16 -0300

    Add files via upload

---
+++ ExercicioOO12_LambdaProducts/ExercicioOO12_LambdaProducts/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioOO12_LambdaProducts.Entities;
+using System.Globalization;
+
+namespace ExercicioOO12_LambdaProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter the file full path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(",");
+                    string name = fields[0];
+                    double price = Double.Parse(fields[1], ci);
+                    list.Add(new Product(name, price));
+                }
+            }
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Average Price = {avg.ToString("f2", ci)}");
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
